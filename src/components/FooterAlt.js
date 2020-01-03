6ae5c7e1ebdc82778870500d3a14706a59cae507
Chris McGivney <cmmcgivney84@gmail.com>
2020-01-03 13:39:20 -0700

    clean up and api key heroku config

--- src/components/FooterAlt.js
+++ src/components/FooterAlt.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Header, Menu} from 'semantic-ui-react'
+import {Header,} from 'semantic-ui-react'
 import SocialFollow from './SocialFollows'
 
 class Footer extends React.Component {
--- src/components/SocialFollows.js
+++ src/components/SocialFollows.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {Header} from "semantic-ui-react"
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {faTwitter, faInstagram, faFacebook} from "@fortawesome/free-brands-svg-icons"
 
