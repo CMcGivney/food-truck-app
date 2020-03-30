4d2582ba717a448aab7a33455a8308855558e79e
Chris McGivney <cmmcgivney84@gmail.com>
2020-03-30 16:57:50 -0600

    ahh style and layout, fix one thing change another

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,13 +3,11 @@ import { Slide } from 'react-slideshow-image'
 import FoamCaviar from '../Images/foamCaviar.jpeg'
 import salmonCuc from '../Images/salmonCuc.jpeg'
 import shavedSalad from '../Images/shavedSalad.jpeg'
-import amaebi from '../Images/amaebi.jpeg'
 import bolinese from '../Images/bolinese.jpeg'
 import nicoiseSpin from '../Images/nicoiseSpin.jpeg'
 import saladOne from '../Images/saladOne.jpeg'
 import tunaCan from '../Images/tunaCan.jpeg'
 import yingYangSoup from '../Images/yingYangSoup.jpeg'
-import tomatoSalad from '../Images/tomatoSalad.jpeg'
 import peachSalad from '../Images/peachSalad.jpeg'
 import chartBoard from '../Images/chartBoard.jpeg'
 import dessertOne from '../Images/dessertOne.jpeg'
@@ -17,9 +15,10 @@ import dessertTwo from '../Images/dessertTwo.jpeg'
 import dessertThree from '../Images/dessertThree.jpeg'
 
  
+
 const slideImages = [
-  FoamCaviar, salmonCuc, shavedSalad, amaebi, bolinese, nicoiseSpin,
-  saladOne, tunaCan, yingYangSoup, tomatoSalad, peachSalad, chartBoard,
+  FoamCaviar, salmonCuc, shavedSalad, bolinese, nicoiseSpin,
+  saladOne, tunaCan, yingYangSoup, peachSalad, chartBoard,
   dessertOne, dessertTwo, dessertThree,
 ];
  
@@ -40,7 +39,6 @@ const properties = {
         <Slide {...properties}>
           <div className="each-slide">
             <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
-              
             </div>
           </div>
           <div className="each-slide">
@@ -100,16 +98,6 @@ const properties = {
           </div>
           <div className="each-slide">
             <div style={{'backgroundImage': `url(${slideImages[12]})`}}>
-              
-            </div>
-          </div>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[13]})`}}>
-              
-            </div>
-          </div>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[14]})`}}>
               
             </div>
           </div>
